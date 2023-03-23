4647204708e59496928c9abaa35ea921f9f3284b
DenishMori <denishnori37@gmail.com>
2023-03-23 10:02:02 +0530

    first commit

---
+++ admin/home.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+   <body>
+
+ 
+      
+
+   </html>
+
+<!--h1 style="margin-top:98px">Welcome back <b--><!--?php echo $_SESSION['adminusername']; ?></b></h1-->
+<!--div class="bg-dashboard">
+<section class="dashboard">
+   <center><h1 class="title">Dashboard</h1></center>
+
+   <div class="box-container">
+
+   <div class="box">
+      <!-?php
+         $select_pizza = $conn->prepare("SELECT * FROM `pizza`");
+         $select_pizza->execute();
+      ?>
+      <p>products added</p>
+      <a href="index.php?page=menuManage" class="btn">see products</a>
+      </div-->
---
+++ footer.php
@@ -0,0 +1,34 @@
+<footer class="footer">
+
+   <section class="box-container" align ="center">
+
+      <div class="box" >
+         <h3>quick links</h3>
+         <a href="index.php"> <i class="fas fa-angle-right"></i>      home</a>
+         <a href="viewcart.php"> <i class="fas fa-angle-right"></i>    cart</a>
+         <a href="viewProfile.php"> <i class="fas fa-angle-right"></i> profile</a>
+         <a href="about.php"> <i class="fas fa-angle-right"></i>about</a>
+         <a href="contact.php"> <i class="fas fa-angle-right"></i>contact</a>
+      </div>
+
+      <div class="box">
+         <h3>contact info</h3>
+         <p> <i class="fas fa-phone"></i> +123-456-7890 </p>
+         <p> <i class="fas fa-phone"></i> +111-222-3333 </p>
+         <p> <i class="fas fa-envelope"></i> shaikhanas@gmail.com </p>
+         <p> <i class="fas fa-map-marker-alt"></i> mumbai, india - 400104 </p>
+      </div>
+
+      <div class="box">
+         <h3>follow us</h3>
+         <a href="#"> <i class="fab fa-facebook-f"></i> facebook </a>
+         <a href="#"> <i class="fab fa-twitter"></i> twitter </a>
+         <a href="#"> <i class="fab fa-instagram"></i> instagram </a>
+         <a href="#"> <i class="fab fa-linkedin"></i> linkedin </a>
+      </div>
+
+   </section>
+
+   <p class="credit"> <span>GROCCETTO FOR YOUR HELTHY FOOD!</span>  </p>
+
+</footer>
